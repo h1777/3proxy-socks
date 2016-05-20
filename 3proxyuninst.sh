df9ca2ea322565e625b5aec2e03dd661cd2d95f4
Baran Kılıç <barankilic@gmail.com>
2016-05-20 15:32:56 +0300

    Update

--- 3proxyuninst.sh
+++ 3proxyuninst.sh
@@ -1,10 +1,10 @@
 /etc/init.d/3proxyinit stop
 rm -rf /etc/3proxy
-rm /etc/rc0.d/K203proxyinit
-rm /etc/rc1.d/K203proxyinit 
-rm /etc/rc6.d/K203proxyinit
-rm /etc/rc2.d/S203proxyinit
-rm /etc/rc3.d/S203proxyinit 
-rm /etc/rc4.d/S203proxyinit
-rm /etc/rc5.d/S203proxyinit
+rm /etc/rc0.d/*proxyinit
+rm /etc/rc1.d/*proxyinit 
+rm /etc/rc6.d/*proxyinit
+rm /etc/rc2.d/*proxyinit
+rm /etc/rc3.d/*proxyinit 
+rm /etc/rc4.d/*proxyinit
+rm /etc/rc5.d/*proxyinit
 rm /etc/init.d/3proxyinit
