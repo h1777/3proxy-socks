8ad1239e9711e1af44c13d1fe0f54e738a5d7e3f
Helder <trentjnr@aol.com>
2017-03-06 17:34:28 +0000

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -8,11 +8,11 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/h1777/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/h1777/3proxy/raw/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/barankilic/3proxy/master/3proxyinit
+wget --no-check-certificate https://raw.github.com/h1777/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
