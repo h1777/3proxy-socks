84761f1973c4ad93356c9be2d8ab12a3f35d7bbf
Helder <trentjnr@aol.com>
2018-04-09 13:49:22 +0100

    Upgraded version 0.8.11

--- 3proxy_installerX.sh
+++ 3proxy_installerX.sh
@@ -1,8 +1,8 @@
 apt-get update && apt-get -y upgrade
 apt-get install -y build-essential nano
-wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.10.tar.gz
-tar xzf 0.8.10.tar.gz
-cd 3proxy-0.8.10
+wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.11.tar.gz
+tar xzf 0.8.11.tar.gz
+cd 3proxy-0.8.11
 make -f Makefile.Linux
 cd src
 mkdir /etc/3proxy/
