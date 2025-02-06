14a614c35f3a384f212ac2dc83b7d75c40d427d1
Helder <PrestonBen@pm.me>
2025-02-06 12:27:26 +0000

    Update and rename 3proxyinstaller.sh to 3proxy.sh

---
+++ 3proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#apt-get update && apt-get -y upgrade
+#apt-get install -y build-essential nano
+sudo apt-get --assume-yes update && sudo apt-get --assume-yes install build-essential && sudo apt --assume-yes install git
+git clone https://github.com/z3apa3a/3proxy
+cd 3proxy
+ln -s Makefile.Linux Makefile
+make
+sudo make install
+chmod 755 /etc/3proxy/conf/add3proxyuser.sh
+cd /usr/local/3proxy/conf/
+sudo rm 3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/h1777/3proxy-socks/master/3proxy.cfg
+chmod 755 /etc/3proxy/3proxy.cfg
